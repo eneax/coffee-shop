9c2f90d7fe653b59c729e89c97e0df960c6dd473
eneax <19818937+eneax@users.noreply.github.com>
2019-02-19 09:48:24 +0100

    Create navbar component

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,10 +2,11 @@ import React from "react"
 import PropTypes from "prop-types"
 
 import "./bootstrap.min.css"
-import "./layout.css"
+import Navbar from './navbar'
 
 const Layout = ({ children }) => (
   <>
+    <Navbar />
     { children }
   </>
 )
