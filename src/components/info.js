f6f3abbc0a2b6519937aa26cf0506859a320603d
eneax <19818937+eneax@users.noreply.github.com>
2019-02-19 18:33:35 +0100

    Create title component

--- src/components/info.js
+++ src/components/info.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Title from './title'
 
 const Info = () => (
   <section className='py-5'>
     <div className='container'>
       <div className='col-10 col-sm-8 mx-auto text-center'>
+        <Title title='our story' />
         <p className='lead text-muted mb-5'>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis laborum quae accusamus sunt odio, eligendi natus dolorem adipisci dolore quo facere delectus minima molestiae, nihil quos eveniet? Eveniet, perspiciatis tenetur.
         </p>
