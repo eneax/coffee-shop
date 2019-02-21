1a41144b91b55ec1abff0b603f94dba3a0cd712d
eneax <19818937+eneax@users.noreply.github.com>
2019-02-21 15:31:44 +0100

    Display coffee items

--- src/components/menu.js
+++ src/components/menu.js
@@ -12,16 +12,33 @@ export default class Menu extends Component {
       coffeeItem: props.items.edges
     }
   }
+
   render() {
     if (this.state.items.length > 0) {
       return (
         <section className='menu py-5'>
           <div className='container'>
             <Title title='best of our menu' />
-            <div className='row mb-5'>
-              <div className='col-10'>
-                <h1>these are our items</h1>
-              </div>
+            {/* categories */}
+            {/* items */}
+            <div className='row'>
+              {this.state.coffeeItem.map(({ node }) => (
+                <div key={node.id} className='col-11 col-md-6 my-3 d-flex mx-auto'>
+                  <div>
+                    <Img fixed={node.image.fixed} />
+                  </div>
+                  {/* items */}
+                  <div className='flex-grow-1 px-3'>
+                    <div className="d-flex justify-content-between">
+                      <h6 className="mb-0">{node.title}</h6>
+                      <h6 className="mb-0">${node.price}</h6>
+                    </div>
+                    <p className="text-muted">
+                      <small>{node.description.description}</small>
+                    </p>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </section>
