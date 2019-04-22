4110292d0b8eb3fdc905cac0e4e41708e1d02d06
eneax <19818937+eneax@users.noreply.github.com>
2019-04-22 23:09:03 +0200

    Refactor headings

---
+++ src/components/headings.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const HeadingOne = ({ header }) => (
+  <div className='row'>
+    <div className="col text-center mb-3">
+      <h1 className='display-4 text-capitalize font-weight-bold'>
+        {header}
+      </h1>
+    </div>
+  </div>
+)
+
+export const HeadingTwo = ({ header }) => (
+  <div className='row'>
+    <div className="col text-center mb-3">
+      <h1 className='display-5 text-capitalize font-weight-bold'>
+        {header}
+      </h1>
+    </div>
+  </div>
+)
