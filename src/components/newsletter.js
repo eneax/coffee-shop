5af7cd9be56008c03f9f854da59e8cb4056e5803
eneax <19818937+eneax@users.noreply.github.com>
2019-04-23 21:40:29 +0200

    Refactor newsletter

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -1,59 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
 import { HeadingTwo } from './headings'
+import { Container, InfoWrapper } from '../components/styles/globalStyles'
 
 const InfoSection = styled.section`
   padding-top: 3rem;
   padding-bottom: 3rem;
-`
-
-const Container = styled.div`
-  width: 100%;
-  padding-right: 15px;
-  padding-left: 15px;
-  margin-right: auto;
-  margin-left: auto;
-
-  /* media */
-  @media (min-width: 576px) {
-    max-width: 540px;
-  }
-  @media (min-width: 768px) {
-    max-width: 720px;
-  }
-  @media (min-width: 992px) {
-    max-width: 960px;
-  }
-  @media (min-width: 1200px) {
-    max-width: 1140px;
-  }
-`
-
-const InfoWrapper = styled.div`
-  position: relative;
-  width: 100%;
-  padding-right: 15px;
-  padding-left: 15px;
-  -ms-flex: 0 0 83.333333%;
-  flex: 0 0 83.333333%;
-  max-width: 83.333333%;
-
-  @media (min-width: 576px) {
-    -ms-flex: 0 0 66.666667%;
-    flex: 0 0 66.666667%;
-    max-width: 66.666667%;
-  }
-
-  margin-right: auto;
-  margin-left: auto;
-  text-align: center;
-
-  p {
-    font-size: 1.25rem;
-    font-weight: 300;
-    color: #6c757d;
-    margin-bottom: 3rem;
-  }
 `
 
 const Input = styled.input`
