12f8d1cb72297424540651a52508ab0895366bcb
eneax <19818937+eneax@users.noreply.github.com>
2020-04-07 15:29:56 +0200

    Install material-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,6 +60,14 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-material-ui`,
+      options: {
+        stylesProvider: {
+          injectFirst: true,
+        },
+      },
+    },
     `gatsby-plugin-styled-components`,
   ],
 };
