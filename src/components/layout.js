f889e6b8cb079d3dd80831e620c28b4b02d9794a
eneax <19818937+eneax@users.noreply.github.com>
2020-04-07 18:05:35 +0200

    Refactor Layout to use material-ui

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
+import defaultTheme from '../theme';
 import './bootstrap.min.css';
 import './layout.css';
 import Navbar from './navbar';
 import Footer from './footer';
-import { GlobalStyle } from './styles/globalStyles';
 
 const Layout = ({ children }) => (
-  <>
+  <ThemeProvider theme={defaultTheme}>
     <CssBaseline />
-    <GlobalStyle />
+
     <Navbar />
     {children}
     <Footer />
-  </>
+  </ThemeProvider>
 );
 
 Layout.propTypes = {
