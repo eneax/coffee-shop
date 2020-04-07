5c26fd675df726b270cd0b1b8bb65b70552d962e
eneax <19818937+eneax@users.noreply.github.com>
2020-04-07 15:41:15 +0200

    Add CssBaseline

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import './bootstrap.min.css';
 import './layout.css';
@@ -9,8 +10,9 @@ import { GlobalStyle } from './styles/globalStyles';
 
 const Layout = ({ children }) => (
   <>
-    <Navbar />
+    <CssBaseline />
     <GlobalStyle />
+    <Navbar />
     {children}
     <Footer />
   </>
