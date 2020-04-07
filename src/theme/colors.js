e25d9c8806dc0956eaf71759ecadbb2a6550030d
eneax <19818937+eneax@users.noreply.github.com>
2020-04-07 18:04:33 +0200

    Add theme colors

---
+++ src/theme/colors.js
@@ -0,0 +1,17 @@
+export const primary = '#6200ee';
+export const primaryLight = '#9e47ff';
+export const primaryDark = '#0400ba';
+
+export const secondary = '#03d8c3';
+export const secondaryLight = '#65fff6';
+export const secondaryDark = '#00a693';
+
+export const primaryTextColor = '#fff';
+export const secondaryTextColor = '#000';
+
+export const infoColor = '#2196f3';
+export const successColor = '#4caf50';
+export const warningColor = '#ff9800';
+export const errorColor = '#f44336';
+
+export const primaryBackground = '#F2F2F2';
