5a9e66d933df1bb889c6b9e554ba11a9246bed2e
eneax <19818937+eneax@users.noreply.github.com>
2020-04-11 17:58:05 +0200

    Refactor homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import BackgroundSection from '../components/bgSection';
+import Hero from '../components/hero';
 import Info from '../components/info';
 import Menu from '../components/menu';
 import Newsletter from '../components/newsletter';
 
-const IndexPage = ({ data }) => (
+const Homepage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <BackgroundSection
-      img={data.bgImg.childImageSharp.fluid}
-      title="welcome to joe's"
-      styleClass="default-background"
-    />
+    <Hero img={data.bgImg.childImageSharp.fluid} />
     <Info />
     <Menu items={data.menu} />
     <Newsletter />
   </Layout>
 );
 
-export default IndexPage;
+export default Homepage;
 
 export const query = graphql`
   {
