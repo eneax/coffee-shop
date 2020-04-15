9ddc5671b10101336f9111f4efc8bf92544c0309
eneax <19818937+eneax@users.noreply.github.com>
2020-04-15 17:51:01 +0200

    Decrease padding for menu grid

--- src/components/menu/menuTabs.js
+++ src/components/menu/menuTabs.js
@@ -94,7 +94,7 @@ const ScrollableTabs = ({ menu }) => {
 
       {/* menu items */}
       <Container className={classes.cardGrid} maxWidth="md">
-        <Grid container spacing={4}>
+        <Grid container spacing={0}>
           {coffeeItems.map(({ node }) => (
             <Grid item key={node.id} xs={12} sm={6} md={4}>
               <TabPanel value={value} index={value}>
