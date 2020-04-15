f39c2e26c311d2f71e0d834ef67c22770a7e8274
eneax <19818937+eneax@users.noreply.github.com>
2020-04-15 17:37:08 +0200

    Add PropTypes to CustomCard

--- src/components/card.js
+++ src/components/card.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
 
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+
+import { fluidObject } from '../utils';
 
 const useStyles = makeStyles(theme => ({
   card: {
@@ -43,6 +46,13 @@ const CustomCard = ({ img, title, price, description }) => {
       </CardActions>
     </Card>
   );
+};
+
+CustomCard.propTypes = {
+  img: fluidObject.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
 };
 
 export default CustomCard;
