352fa6b985a994e73cee9bb2535f3f6668015abb
eneax <19818937+eneax@users.noreply.github.com>
2020-06-04 21:07:58 +0200

    Create header component

---
+++ src/components/header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import { FiCoffee, FiShoppingBag } from 'react-icons/fi';
+
+const navItems = ['About', 'Contact', 'Shop'];
+
+const Header = () => (
+  <header className="text-gray-500 bg-gray-900">
+    <div className="mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <Link
+        to="/"
+        className="flex font-medium items-center text-white mb-4 md:mb-0 hover:cursor-pointer"
+      >
+        <FiCoffee className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" />
+        <span className="ml-3 text-xl">Fairy Cake</span>
+      </Link>
+
+      <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+        {navItems.map(item => (
+          <Link
+            key={item}
+            to={`/${item.toLowerCase()}`}
+            className="mr-5 hover:text-white"
+          >
+            {item}
+          </Link>
+        ))}
+      </nav>
+
+      <FiShoppingBag className="w-4 h-4 ml-1 hover:cursor-pointer snipcart-checkout" />
+    </div>
+  </header>
+);
+
+export default Header;
