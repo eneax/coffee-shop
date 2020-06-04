e37c53f95ee7bd1993cd5503767c7234e2ff0cc6
eneax <19818937+eneax@users.noreply.github.com>
2020-06-04 21:11:04 +0200

    Reorganize layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-
-import defaultTheme from '../theme';
-import './bootstrap.min.css';
+import Header from './header';
 import './layout.css';
-import Navbar from './navbar/navbar';
-import Footer from './footer';
 
 const Layout = ({ children }) => (
-  <ThemeProvider theme={defaultTheme}>
-    <CssBaseline />
-
-    <Navbar />
+  <>
+    <Header />
     {children}
-    <Footer />
-  </ThemeProvider>
+  </>
 );
 
 Layout.propTypes = {
