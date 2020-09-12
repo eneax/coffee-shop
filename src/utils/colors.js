75b91991162efb7a1e928ef95333c7d9bbc19614
eneax <19818937+eneax@users.noreply.github.com>
2020-09-12 17:47:53 +0200

    Delete util folder

--- src/utils/colors.js
+++
@@ -1,7 +0,0 @@
-export const grey = '#555';
-export const darkGrey = '#333';
-export const pueblo = '#7D160F';
-
-export const facebook = '#3B5997';
-export const twitter = '#47A0EB';
-export const mail = '#DD5347';
