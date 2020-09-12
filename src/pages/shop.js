84594487a4cfe945bc15e13e1cc71004a9bd69a2
eneax <19818937+eneax@users.noreply.github.com>
2020-09-12 17:49:29 +0200

    Refactor shop page to query for yaml data

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,33 +1,47 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import BackgroundSection from '../components/bgSection';
+import Hero from '../components/hero';
 import Products from '../components/products';
 
-const Shop = ({ data }) => (
-  <Layout>
-    <SEO title="Shop" keywords={[`gatsby`, `application`, `react`]} />
-    <BackgroundSection
-      img={data.bgImg.childImageSharp.fluid}
-      title="Hand Roasted Coffee"
-      styleClass="about-background"
-    />
-    <Products />
-  </Layout>
-);
-
-export default Shop;
-
-export const query = graphql`
+const getShopData = graphql`
   {
-    bgImg: file(relativePath: { eq: "about-background.jpeg" }) {
-      childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid
+    shopYaml {
+      shop {
+        hero {
+          bgImg {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          mainHeading
+          text
         }
       }
     }
   }
 `;
+
+const Shop = () => {
+  const response = useStaticQuery(getShopData);
+  const {
+    shopYaml: {
+      shop: { hero },
+    },
+  } = response;
+
+  return (
+    <Layout>
+      <SEO title="Shop" />
+
+      <Hero {...hero} />
+      <Products />
+    </Layout>
+  );
+};
+
+export default Shop;
