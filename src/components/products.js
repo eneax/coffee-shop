c45bf9c10cbe6d1c117467bce5f735e2f6231c35
eneax <19818937+eneax@users.noreply.github.com>
2020-09-12 13:44:34 +0200

    Refactor products with tailwind

--- src/components/products.js
+++ src/components/products.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { StaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import Product from './product';
-import { HeadingOne } from './headings';
 
-const PRODUCTS_QUERY = graphql`
+const getProducts = graphql`
   {
     products: allContentfulCoffeeProduct {
       edges {
@@ -24,25 +23,59 @@ const PRODUCTS_QUERY = graphql`
   }
 `;
 
-const Products = () => (
-  <StaticQuery
-    query={PRODUCTS_QUERY}
-    render={data => (
-      <section className="py-5">
-        <div className="container">
-          <HeadingOne header="our products" />
-          <div className="row">
-            {data.products.edges.map(({ node: product }) => (
-              <Product
-                key={product.id}
-                product={product} // we pass the entire node to the product component
-              />
-            ))}
+const Products = () => {
+  const response = useStaticQuery(getProducts);
+  const { products } = response;
+
+  return (
+    <section className="relative py-20">
+      <div
+        className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
+        style={{ height: '80px', transform: 'translateZ(0px)' }}
+      >
+        <svg
+          className="absolute bottom-0 overflow-hidden"
+          xmlns="http://www.w3.org/2000/svg"
+          preserveAspectRatio="none"
+          version="1.1"
+          viewBox="0 0 2560 100"
+          x="0"
+          y="0"
+        >
+          <polygon
+            className="text-white fill-current"
+            points="2560 0 2560 100 0 100"
+          />
+        </svg>
+      </div>
+      <div className="container mx-auto px-4">
+        <div className="flex flex-wrap justify-center text-center mb-12 lg:mb-24">
+          <div className="w-full lg:w-6/12 px-4">
+            <h2 className="text-4xl font-semibold">Best Of Our Menu</h2>
+            <p className="text-lg leading-relaxed m-4 text-gray-600">
+              Enjoy the best products in town!
+            </p>
           </div>
         </div>
-      </section>
-    )}
-  />
-);
+
+        <div className="flex flex-wrap">
+          <div className="w-full">
+            <div className="relative flex flex-col min-w-0 w-full mb-6">
+              <div className="py-5 flex-auto">
+                <div className="tab-content tab-space">
+                  <div className="flex flex-wrap">
+                    {products.edges.map(({ node: product }) => (
+                      <Product key={product.id} product={product} />
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Products;
