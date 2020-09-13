6eb522cc5c5c04ffd3b85816eb3629720684453a
eneax <19818937+eneax@users.noreply.github.com>
2020-09-13 11:58:09 +0200

    Update contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -39,57 +39,65 @@ const Contact = () => (
           <div className="w-full lg:w-6/12 px-4">
             <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300">
               <div className="flex-auto p-5 lg:p-8">
-                <div className="relative w-full mb-3">
-                  <label
-                    className="block uppercase text-gray-700 text-xs font-bold"
-                    htmlFor="full-name"
-                  >
-                    Full Name
-                    <input
-                      type="text"
-                      className="mt-2 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-                      placeholder="Full Name"
-                      style={{ transition: 'all 0.15s ease 0s;' }}
-                    />
-                  </label>
-                </div>
-                <div className="relative w-full mb-3">
-                  <label
-                    className="block uppercase text-gray-700 text-xs font-bold"
-                    htmlFor="email"
-                  >
-                    Email
-                    <input
-                      type="email"
-                      className="mt-2 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-                      placeholder="Email"
-                      style={{ transition: 'all 0.15s ease 0s;' }}
-                    />
-                  </label>
-                </div>
-                <div className="relative w-full mb-3">
-                  <label
-                    className="block uppercase text-gray-700 text-xs font-bold"
-                    htmlFor="message"
-                  >
-                    Message
-                    <textarea
-                      rows="4"
-                      cols="80"
-                      className="mt-2 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-                      placeholder="Type a message..."
-                    />
-                  </label>
-                </div>
-                <div className="text-center mt-6">
-                  <button
-                    className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-                    type="button"
-                    style={{ transition: 'all 0.15s ease 0s;' }}
-                  >
-                    Send Message
-                  </button>
-                </div>
+                <form action="POST" data-netlify="true">
+                  <div className="relative w-full mb-3">
+                    <label
+                      className="block uppercase text-gray-700 text-xs font-bold"
+                      htmlFor="full-name"
+                    >
+                      Full Name
+                      <input
+                        type="text"
+                        className="mt-2 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                        name="name"
+                        id="name"
+                        placeholder="Full Name"
+                        style={{ transition: 'all 0.15s ease 0s' }}
+                      />
+                    </label>
+                  </div>
+                  <div className="relative w-full mb-3">
+                    <label
+                      className="block uppercase text-gray-700 text-xs font-bold"
+                      htmlFor="email"
+                    >
+                      Email
+                      <input
+                        type="email"
+                        className="mt-2 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                        name="email"
+                        id="email"
+                        placeholder="Email"
+                        style={{ transition: 'all 0.15s ease 0s' }}
+                      />
+                    </label>
+                  </div>
+                  <div className="relative w-full mb-3">
+                    <label
+                      className="block uppercase text-gray-700 text-xs font-bold"
+                      htmlFor="message"
+                    >
+                      Message
+                      <textarea
+                        rows="4"
+                        cols="80"
+                        name="message"
+                        id="message"
+                        placeholder="Type a message..."
+                        className="mt-2 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                      />
+                    </label>
+                  </div>
+                  <div className="text-center mt-6">
+                    <button
+                      className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+                      type="button"
+                      style={{ transition: 'all 0.15s ease 0s' }}
+                    >
+                      Send Message
+                    </button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
