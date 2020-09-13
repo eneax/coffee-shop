cfb341f694ba17d17b55fdbfbc7c24de39bba5f6
eneax <19818937+eneax@users.noreply.github.com>
2020-09-13 11:25:00 +0200

    Style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
@@ -6,8 +7,18 @@ import SEO from '../components/seo';
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+
+    <div
+      className="flex items-center bg-gray-900 text-white"
+      style={{ minHeight: 'calc(100vh - 10rem)' }}
+    >
+      <div className="mx-auto">
+        <h1 className="text-white font-semibold text-5xl">NOT FOUND</h1>
+        <Link to="/" className="font-bold text-gray-100 mt-8">
+          <span className="flex items-center">Back home &#8594;</span>
+        </Link>
+      </div>
+    </div>
   </Layout>
 );
 
